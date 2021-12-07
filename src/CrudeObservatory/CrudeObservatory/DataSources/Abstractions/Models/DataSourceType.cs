a27f05c8d0b94f2e9afdd119a55f5858b59d3a8e
jkoplo <jkoplo@users.noreply.github.com>
2021-12-06 22:28:35 -0700

    Framing out DataSources

---
+++ src/CrudeObservatory/CrudeObservatory/DataSources/Abstractions/Models/DataSourceType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudeObservatory.DataSources.Abstractions.Models
+{
+    public enum DataSourceType
+    {
+        libplctag
+    }
+}
---
+++ src/CrudeObservatory/CrudeObservatory/DataSources/Implementations/Libplctag/Models/DataSourceConfig.cs
@@ -0,0 +1,14 @@
+﻿using CrudeObservatory.DataSources.Abstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudeObservatory.DataSources.Implementations.Libplctag.Models
+{
+    public class DataSourceConfig : DataSourceConfigBase
+    {
+        public string TagPath { get; set; }
+    }
+}
