52ee41b7f28aa763518a4e1e12451e29542fb346
jkoplo <jkoplo@users.noreply.github.com>
2021-12-06 22:33:40 -0700

    Framing out DataTargets

---
+++ src/CrudeObservatory/CrudeObservatory/DataTargets/Implementations/CSV/Models/DataTargetConfig.cs
@@ -0,0 +1,13 @@
+﻿using CrudeObservatory.DataTargets.Abstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudeObservatory.DataTargets.Implementations.CSV.Models
+{
+    public class DataTargetConfig : DataTargetConfigBase
+    {
+    }
+}
