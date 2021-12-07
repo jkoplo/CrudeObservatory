e6f5ab4bbc970b83813b317ee9bdb4f2ca999236
jkoplo <jkoplo@users.noreply.github.com>
2021-12-06 22:48:31 -0700

    Comment logical flow into worker

--- src/CrudeObservatory/CrudeObservatory/Worker.cs
+++ src/CrudeObservatory/CrudeObservatory/Worker.cs
@@ -11,8 +11,19 @@ namespace CrudeObservatory
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            //Pull in config - might be at the Program.cs DI level
+            //Build out classes - might be at the Program.cs DI level
+
+            //Initiate connections? Could be DataSources (PLC) or DataTargets (DB)
+            //Wait for start trigger
+
             while (!stoppingToken.IsCancellationRequested)
             {
+                //Wait for interval OR end trigger
+                //Get data from source(s)
+                //Write data to target(s)
+                //Repeat @ Wait for interval OR end trigger
+
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
