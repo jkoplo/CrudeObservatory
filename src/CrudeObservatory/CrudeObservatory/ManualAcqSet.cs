cf8fc6630324ca68310d16471868afe9399a80c1
jkoplo <jkoplo@users.noreply.github.com>
2021-12-15 23:40:27 -0700

    Actual change to make ManualAcqSet be two pieces

--- src/CrudeObservatory/CrudeObservatory/ManualAcqSet.cs
+++ src/CrudeObservatory/CrudeObservatory/ManualAcqSet.cs
@@ -16,24 +16,45 @@ using CrudeObservatory.Triggers.Implementations.Delay.Models;
 using CrudeObservatory.Triggers.Abstractions.Models;
 using CrudeObservatory.DataTargets.Implementations.CSV;
 using CrudeObservatory.DataTargets.Implementations.CSV.Models;
+using CrudeObservatory.DataSources.Abstractions.Models;
 
 namespace CrudeObservatory
 {
     internal static class ManualAcqSet
     {
+        internal static AcquisitionConfig GetAcquisitionConfig()
+        {
+            AcquisitionConfig acq = new AcquisitionConfig();
+
+            acq.Name = "Manual Prototype Set";
+            acq.StartTrigger = new TriggerConfigBase() { Type = TriggerType.Auto };
+            acq.DataSources = new List<DataSourceConfigBase>()
+            {
+                new SineWaveDataSourceConfig(){PeriodSec=5, Alias="SineWave" }
+            };
+            acq.Interval = new FixedIntervalConfig() { PeriodSec = .1, Type = IntervalType.Fixed };
+            acq.EndTrigger = new DelayTriggerConfig() { Type = TriggerType.Delay, DelaySeconds = 10, Enabled = true };
+            acq.DataTarget = new CsvDataTargetConfig() { FilePath = Path.Combine(System.Environment.CurrentDirectory, "DataTarget.csv") };
+
+            return acq;
+
+        }
+
         internal static AcquisitionSet GetAcquisition()
         {
+            var config = ManualAcqSet.GetAcquisitionConfig();
+
             AcquisitionSet acq = new AcquisitionSet();
 
             acq.Name = "Manual Prototype Set";
             acq.StartTrigger = new AutoTrigger();
             acq.DataSources = new List<IDataSource>()
             {
-                new SineWaveDataSource(new SineWaveDataSourceConfig(){PeriodSec=5, Alias="SineWave" })
+                new SineWaveDataSource(config.DataSources.OfType<SineWaveDataSourceConfig>().First())
             };
-            acq.Interval = new FixedInterval(new FixedIntervalConfig() { PeriodSec = .1, Type = IntervalType.Fixed });
-            acq.EndTrigger = new DelayTrigger(new DelayTriggerConfig() { Type = TriggerType.Delay, DelaySeconds = 10, Enabled = true });
-            acq.DataTarget = new CsvDataTarget(new CsvDataTargetConfig() { FilePath = Path.Combine(System.Environment.CurrentDirectory, "DataTarget.csv") });
+            acq.Interval = new FixedInterval((FixedIntervalConfig)config.Interval);
+            acq.EndTrigger = new DelayTrigger((DelayTriggerConfig)config.EndTrigger);
+            acq.DataTarget = new CsvDataTarget((CsvDataTargetConfig)config.DataTarget);
 
             return acq;
 
