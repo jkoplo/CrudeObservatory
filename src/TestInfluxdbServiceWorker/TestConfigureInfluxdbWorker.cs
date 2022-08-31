63b975c0712349cff10274160f7174d32540b407
jkoplo <jkoplo@users.noreply.github.com>
2022-08-30 21:43:06 -0700

    Generate sine waves for fake data

--- src/TestInfluxdbServiceWorker/TestConfigureInfluxdbWorker.cs
+++ src/TestInfluxdbServiceWorker/TestConfigureInfluxdbWorker.cs
@@ -41,33 +41,49 @@ namespace TestInfluxdbServiceWorker
             //};
 
             var client = InfluxDBClientFactory.Create(dbConfig.Url, dbConfig.Token);
+            using var writeApi = client.GetWriteApi();
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Delay(500);
+                await Task.Delay(100);
                 DateTimeOffset dateTimeOffset = DateTimeOffset.Now;
+
+                var sineWave = SineWave();
 
                 var points = new List<PointData>
                 {
                     PointData.Builder
                         .Measurement("TestMeasurement")
                         //.Tag("host", "host2")
-                        .Field("First", 12345)
+                        .Field("First", sineWave)
                         .Timestamp(dateTimeOffset.UtcDateTime, WritePrecision.Ns)
                         .ToPointData(),
                     PointData.Builder
                         .Measurement("TestMeasurement")
                         //.Tag("host", "host2")
-                        .Field("Second", 54321)
+                        .Field("Second", sineWave * -1)
                         .Timestamp(dateTimeOffset.UtcDateTime, WritePrecision.Ns)
                         .ToPointData(),
                 };
 
-                using (var writeApi = client.GetWriteApi())
-                {
-                    writeApi.WritePoints(points, dbConfig.Bucket, dbConfig.Organization);
-                }
+                writeApi.WritePoints(points, dbConfig.Bucket, dbConfig.Organization);
             }
+        }
+
+        private double SineWave()
+        {
+            //https://www.codeproject.com/Articles/30180/Simple-Signal-Generator
+
+            double timeInSeconds = DateTimeOffset.Now.ToUnixTimeMilliseconds() / 1000;
+
+            double rawValue = 0d;
+            double t = .01 * timeInSeconds;
+
+            // sin( 2 * pi * t )
+            rawValue = (double)Math.Sin(2f * Math.PI * t);
+
+            var scaledValue = (100 * rawValue);
+            return scaledValue;
         }
     }
 }
